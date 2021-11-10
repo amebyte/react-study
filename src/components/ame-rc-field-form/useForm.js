7380dd940200a015a9c992020855fa8f7c7ca122
unknown <416212316@qq.com>
2021-11-10 21:34:57 +0800

    formStore

--- src/components/ame-rc-field-form/useForm.js
+++ src/components/ame-rc-field-form/useForm.js
@@ -1,4 +1,34 @@
 import React from "react";
+class FormStore {
+    constructor() {
+        // 数据仓库
+        this.store = {}
+    }
+
+    setFieldValue = (newStore) => {
+        this.store = {
+            ...this.store,
+            ...newStore
+        }
+    }
+
+    getFieldsValue = () => {
+        return { ...this.store }
+    }
+
+    getFieldValue = (name) => {
+        return this.store[name]
+    }
+    // 返回数据仓库的权限
+    getForm = () => {
+        return {
+            setFieldValue: this.setFieldValue,
+            getFieldsValue: this.getFieldsValue,
+            getFieldValue: this.getFieldValue
+        }
+    }
+
+}
 export default function useForm() {
     return []
 }
